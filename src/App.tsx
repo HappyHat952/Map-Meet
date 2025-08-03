8ab0bb7ff3767cf46213da52150e16e3d8dac8ce
NitaraS <senrenjp@gmail.com>
2025-08-03 12:55:34 -0400

    pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import Map from './pages/Map';
+import Profile from './pages/Profile';
+import Project from './pages/Project';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Hello</p>
+        <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element = {<Map/>} />
+        <Route path="/profile" element = {<Profile/>} />
+        <Route path="/project" element = {<Project/>} />
+        <Route path="*" element = {<PageNotFound/>} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Map.tsx
@@ -0,0 +1,9 @@
+// default return Home
+function Map ()
+{
+    return (
+        <p>This is map page.</p>
+    );
+}
+
+export default Map;
