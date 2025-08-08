f588acb18f3c034c93b6a4542d0b6f2ec1d100d3
kwen07 <y.katiewen@gmail.com>
2025-08-07 23:24:43 -0400

    remove the comment button

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -42,13 +42,8 @@ const ProjectDetail = () => {
               </div>
               <div className="image-actions">
                 <button className="image-action-button like-button">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
                     <path d="m8 2.748-.717-.737C5.6.271 2.5 1.755 2.5 4.723c0 1.524.608 2.84 1.561 3.878A12.9 12.9 0 0 0 8 13.593a12.9 12.9 0 0 0 3.939-4.992c.953-1.038 1.56-2.354 1.56-3.878 0-2.968-3.1-4.452-4.783-2.708L8 2.748z"/>
-                  </svg>
-                </button>
-                <button className="image-action-button comment-button">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
                   </svg>
                 </button>
               </div>
